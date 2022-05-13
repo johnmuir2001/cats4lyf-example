cf3a40fe70054cf98f64212368f73cbac551e3f1
John Muir <johnmuir@Johns-Work-Laptop.local>
2022-05-13 22:10:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [allCats, setAllCats] = useState([])
+
+  useEffect(() => {
+    fetchCatData()
+  }, [])
+
+  const fetchCatData = async () => {
+    try {
+      const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=10");
+      const data = await response.json();
+      setAllCats(data);
+    } catch (err){
+      console.log(err.message);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>BONK</h1>
+      {allCats.map((cat, index) => {
+        return <img key={index} src={cat.url} alt={index}/>
+      })}
     </div>
   );
 }
